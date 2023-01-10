98bbb52e6efd99f68c2c68f01d4b4d9afddba5f9
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2023-01-10 01:03:31 -0300

    Add `reallocate_bytes`

--- common/src/mmap.rs
+++ common/src/mmap.rs
@@ -1,5 +1,5 @@
 use core::ptr;
-use libc::{c_void, mmap, munmap, MAP_ANON, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+use libc::{c_void, mmap, munmap, MAP_ANON, MAP_PRIVATE, MREMAP_MAYMOVE, PROT_READ, PROT_WRITE};
 
 /// Request a contiguous and anonymous memory block from the kernel.
 ///
@@ -34,4 +34,37 @@ pub fn allocate_bytes(bytes: usize) -> *mut c_void {
 /// Returns true if the operation succeeds.
 pub fn deallocate_bytes(pos: usize, bytes: usize) -> bool {
     0 == unsafe { munmap(pos as *mut _, bytes) }
+}
+
+/// Attempt to reallocate a previously `mmap`'ed address (such as a pointer returned by [`allocate_bytes`]).
+pub unsafe fn reallocate_bytes(
+    old_addr: *mut c_void,
+    old_size: usize,
+    new_size: usize,
+    can_relocate: bool,
+) -> Option<*mut c_void> {
+    // If MREMAP_MAYMOVE is set, the kernel is allowed to
+    // relocate the previous mapping. If it's not set, we know the previous
+    // mapping will be kept valid.
+    let flags = if can_relocate { MREMAP_MAYMOVE } else { 0 };
+
+    let ret_ptr = unsafe { libc::mremap(old_addr, old_size, new_size, flags) };
+
+    match ret_ptr {
+        libc::MAP_FAILED => None,
+        valid_ptr => Some(valid_ptr),
+    }
+}
+
+pub struct AnonymousMapping {
+    start: usize,
+    end: usize,
+}
+
+impl AnonymousMapping {
+    pub fn bytes(&self) -> usize {
+        self.end - self.start
+    }
+
+    // TODO: rest
 }
